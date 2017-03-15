5e9047450f61a064fad4803e63dd71007133727b
George Taveras <george.taveras1231@gmail.com>
2017-03-15 10:50:44 -0400

    Use const

--- packages/a-css-loader/src/create-class-name-generator.js
+++ packages/a-css-loader/src/create-class-name-generator.js
@@ -3,12 +3,12 @@ const loaderUtils = require('loader-utils');
 module.exports = function createClassNameGenerator(pattern, loaderContext) {
   /* Mostly borrowed fron npm: generic-names */
   return function generate(localName, filepath, css) {
-    var name = pattern.replace(/\[local\]/gi, localName);
-    var loaderOptions = {
+    const name = pattern.replace(/\[local\]/gi, localName);
+    const loaderOptions = {
       content: `${localName}+${css}`
     };
 
-    var genericName = loaderUtils.interpolateName(loaderContext, name, loaderOptions);
+    const genericName = loaderUtils.interpolateName(loaderContext, name, loaderOptions);
 
     return genericName
       .replace(new RegExp('[^a-zA-Z0-9\\-_\u00A0-\uFFFF]', 'g'), '-')
